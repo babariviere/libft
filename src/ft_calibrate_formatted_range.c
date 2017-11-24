cb2570f8352180779bbe4127452017aca4531391
Bastien Riviere <fourdotfiveg@gmail.com>
2017-11-24 10:23:49 +0100

    fix

--- src/ft_calibrate_formatted_range.c
+++ src/ft_calibrate_formatted_range.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 09:46:12 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/24 09:47:19 by briviere         ###   ########.fr       */
+/*   Updated: 2017/11/24 10:23:45 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	ft_calibrate_formatted_range(t_formatted **formatteds, size_t start,
 		size_t end, size_t diff_idx)
 {
-	while (start < end)
+	while (start <= end)
 		ft_calibrate_formatted(formatteds, start++, diff_idx);
 }

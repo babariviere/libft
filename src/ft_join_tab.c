cef3f663076df2462639bf02e89f3ffa3ffa4886
Bastien Riviere <fourdotfiveg@gmail.com>
2017-11-28 09:46:33 +0100

    update join tab

--- src/ft_join_tab.c
+++ src/ft_join_tab.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 17:03:15 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/24 17:18:02 by briviere         ###   ########.fr       */
+/*   Updated: 2017/11/28 09:46:21 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	**ft_join_tab(void **tab1, void **tab2)
 	void	**res;
 	size_t	tab1l;
 	size_t	tab2l;
-	size_t	idx;
 
 	if (tab1 == 0 || tab2 == 0)
 		return (0);
@@ -25,18 +24,8 @@ void	**ft_join_tab(void **tab1, void **tab2)
 	tab2l = ft_tablen(tab2);
 	if ((res = malloc(sizeof(void *) * (tab1l + tab2l + 1))) == 0)
 		return (0);
-	idx = 0;
-	while (idx < tab1l)
-	{
-		res[idx] = tab1[idx];
-		idx++;
-	}
-	idx = 0;
-	while (idx + tab1l < tab2l)
-	{
-		res[idx + tab1l] = tab2[idx];
-		idx++;
-	}
-	res[idx + tab1l] = 0;
+	ft_memcpy(res, tab1, tab1l);
+	ft_memcpy(res + tab1l, tab2, tab2l);
+	res[tab1l + tab2l] = 0;
 	return (res);
 }

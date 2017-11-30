972d680adb809ce2df9e372cefc6ff394299807e
Bastien Riviere <fourdotfiveg@gmail.com>
2017-11-30 23:04:09 +0100

    another fix

--- src/ft_tablen.c
+++ src/ft_tablen.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 17:05:33 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/30 22:59:02 by briviere         ###   ########.fr       */
+/*   Updated: 2017/11/30 23:04:01 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ size_t	ft_tablen(const void *tab, size_t elem)
 {
 	size_t			len;
 	const t_uchar	*tptr;
+	t_uchar			nul[elem];
 
 	len = 0;
 	tptr = tab;
-	while (ft_memcmp(tab + (len * elem), 0, elem) == 0)
+	ft_bzero(nul, elem);
+	while (ft_memcmp(tab + (len * elem), nul, elem) == 0)
 		len++;
 	return (len);
 }

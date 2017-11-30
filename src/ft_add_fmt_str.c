1810d8b163455034a66e8cef6323ea63bbb346fc
Bastien Riviere <fourdotfiveg@gmail.com>
2017-11-30 21:37:09 +0100

    forgot one

--- src/ft_add_fmt_str.c
+++ src/ft_add_fmt_str.c
@@ -6,16 +6,16 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 09:41:57 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/24 09:43:21 by briviere         ###   ########.fr       */
+/*   Updated: 2017/11/30 21:36:53 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_add_formatted_str(t_formatted *formatted, char *str,
+void	ft_add_fmt_str(t_fmt *fmt, char *str,
 		int next_padding)
 {
-	formatted->tab[formatted->idx] = str;
-	formatted->padding[formatted->idx] = next_padding;
-	formatted->idx++;
+	fmt->tab[fmt->idx] = str;
+	fmt->padding[fmt->idx] = next_padding;
+	fmt->idx++;
 }

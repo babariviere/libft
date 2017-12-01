f25d261e83529d24635d71fe55993f4efeab393b
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-01 01:46:45 +0100

    fix

--- src/ft_stoa_human.c
+++ src/ft_stoa_human.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 10:38:40 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/01 01:38:58 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/01 01:46:35 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ static char	*ft_stoa_human_sub(size_t nb, size_t dec, size_t thresh)
 	char	*dec_str;
 	char	*tmp_str;
 
-	nbr_str = ft_itoa(nb);
 	if (nb >= 10 || dec < thresh)
-		return (nbr_str);
-	dec_str = ft_itoa((dec / 100) % 10);
+		return (ft_itoa(nb));
+	dec = dec / 100 + ((dec / 10) >= 5);
+	nbr_str = ft_itoa(nb + (dec == 0));
+	dec_str = ft_itoa(dec % 10);
 	tmp_str = ft_strjoin(nbr_str, ".");
 	free(nbr_str);
 	nbr_str = ft_strjoin(tmp_str, dec_str);

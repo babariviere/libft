d1076148f2ef8d0cc81d8b5af239a7cbf2069140
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-01 01:23:08 +0100

    remove shitty things

--- src/ft_stoa_human_dec.c
+++ src/ft_stoa_human_dec.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_stoa_human_bin.c                                :+:      :+:    :+:   */
+/*   ft_stoa_human_dec.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 10:38:40 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/01 01:07:43 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/01 01:21:44 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_stoa_human_bin_sub(size_t nb, size_t dec)
+static char	*ft_stoa_human_dec_sub(size_t nb, size_t dec)
 {
 	char	*nbr_str;
 	char	*dec_str;
 	char	*tmp_str;
 
 	nbr_str = ft_itoa(nb);
-	if (nb >= 10 || dec < 1024)
+	if (nb >= 10 || dec < 1000)
 		return (nbr_str);
-	dec_str = ft_itoa((dec / 100) % 10);
+	dec_str = ft_itoa((dec / 101) % 10);
 	tmp_str = ft_strjoin(nbr_str, ".");
 	free(nbr_str);
 	nbr_str = ft_strjoin(tmp_str, dec_str);
@@ -29,22 +29,22 @@ static char	*ft_stoa_human_bin_sub(size_t nb, size_t dec)
 	return (nbr_str);
 }
 
-char		*ft_stoa_human_bin(size_t nb)
+char		*ft_stoa_human_dec(size_t nb)
 {
 	char	*nb_str;
 	char	*tmp;
 	char	metric[2];
 	size_t	prev;
 
-	metric[0] = ft_human_metric_bin(nb);
+	metric[0] = ft_human_metric(nb);
 	metric[1] = 0;
 	prev = 0;
-	while (nb >= 1024)
+	while (nb >= 1000)
 	{
 		prev = nb;
-		nb /= 1024;
+		nb /= 1000;
 	}
-	tmp = ft_stoa_human_bin_sub(nb, prev);
+	tmp = ft_stoa_human_dec_sub(nb, prev);
 	nb_str = ft_strjoin(tmp, ft_strdup(metric));
 	free(tmp);
 	return (nb_str);

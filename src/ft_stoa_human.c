4aadad3b33b81cd6ca31d59f80f30db7d668161f
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-01 02:16:12 +0100

    new test

--- src/ft_stoa_human.c
+++ src/ft_stoa_human.c
@@ -6,29 +6,33 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 10:38:40 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/01 02:07:17 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/01 02:15:59 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_stoa_human_sub(size_t nb, size_t dec)
+static char	*ft_stoa_human_sub(float nb)
 {
+	int		intpart;
+	int		fracpart;
 	char	*nbr_str;
-	char	*dec_str;
+	char	*frac_str;
 	char	*tmp_str;
 
-	if (dec == 0)
+	intpart = (int)nb;
+	fracpart = (int)(nb - intpart) * 100;
+	if (fracpart == 0)
 		return (ft_itoa(nb));
-	dec = dec / 100 + ((dec / 10) >= 5);
-	nb += ((dec % 10) == 0);
+	fracpart = (fracpart / 10) + ((fracpart % 10) >= 5);
+	nb += ((fracpart % 10) == 0);
 	if (nb >= 10)
 		return (ft_itoa(nb));
 	nbr_str = ft_itoa(nb);
-	dec_str = ft_itoa(dec % 10);
+	frac_str = ft_itoa(fracpart % 10);
 	tmp_str = ft_strjoin(nbr_str, ".");
 	free(nbr_str);
-	nbr_str = ft_strjoin(tmp_str, dec_str);
+	nbr_str = ft_strjoin(tmp_str, frac_str);
 	free(tmp_str);
 	return (nbr_str);
 }
@@ -38,22 +42,19 @@ char		*ft_stoa_human(size_t nb, int into_bytes)
 	char	*nb_str;
 	char	*tmp;
 	char	metric[2];
-	size_t	prev;
-	size_t	thresh;
+	float	thresh;
+	float	res;
 
 	if (into_bytes)
 		metric[0] = ft_human_metric_bin(nb);
 	else
 		metric[0] = ft_human_metric_dec(nb);
 	metric[1] = 0;
-	prev = 0;
+	res = nb;
 	thresh = (into_bytes ? 1024 : 1000);
-	while (nb >= thresh)
-	{
-		prev = nb;
-		nb /= thresh;
-	}
-	tmp = ft_stoa_human_sub(nb, prev);
+	while (res >= thresh)
+		res /= thresh;
+	tmp = ft_stoa_human_sub(res);
 	nb_str = ft_strjoin(tmp, ft_strdup(metric));
 	free(tmp);
 	return (nb_str);

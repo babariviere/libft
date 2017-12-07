eba17fc2b175b5692f52bfc136cd65d5f6f18674
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-07 13:43:53 +0100

    small fix

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 14:57:07 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/10 17:07:43 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/07 13:43:36 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 != 0 && *s1 == *s2)
+	const t_uchar	*s1ptr;
+	const t_uchar	*s2ptr;
+
+	s1ptr = (const t_uchar *)s1;
+	s2ptr = (const t_uchar *)s2;
+	while (*s1ptr != 0 && *s1ptr == *s2ptr)
 	{
-		s1++;
-		s2++;
+		s1ptr++;
+		s2ptr++;
 	}
-	return (*(const t_uchar *)s1 - *(const t_uchar *)s2);
+	return (*s1ptr - *s2ptr);
 }

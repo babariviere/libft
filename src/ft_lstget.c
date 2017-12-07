d73d4c714140dfb48a50adc49e85ff551fb83fa5
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-07 15:07:08 +0100

    rename

--- src/ft_lstget.c
+++ src/ft_lstget.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstat.c                                         :+:      :+:    :+:   */
+/*   ft_lstget.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 16:42:14 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/17 16:47:40 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/07 15:07:01 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstat(const t_list *lst, size_t idx)
+t_list	*ft_lstget(const t_list *lst, size_t idx)
 {
 	while (idx--)
 		lst = lst->next;

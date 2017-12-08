0f192b4372e34fd7b7c40f215b9dfb52d1338445
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-08 17:12:39 +0100

    fix

--- src/ft_dlstdelone.c
+++ src/ft_dlstdelone.c
@@ -6,13 +6,13 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 14:25:41 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/08 17:03:33 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/08 17:12:32 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_lst **alst, void (*del)(void *, size_t))
+void	ft_dlstdelone(t_dlst **alst, void (*del)(void *, size_t))
 {
 	del((*alst)->content, (*alst)->content_size);
 	ft_memdel((void **)alst);

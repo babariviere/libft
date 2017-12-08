bac690adb42fb5b2a33ca0ff67be3618ea589fbc
Bastien Riviere <fourdotfiveg@gmail.com>
2017-12-08 09:16:03 +0100

    small opti

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 14:24:00 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/10 15:24:44 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/08 09:15:42 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(const void *content, size_t content_size)
 {
 	t_list	*node;
 
-	if ((node = ft_memalloc(sizeof(t_list))) == 0)
+	if ((node = malloc(sizeof(t_list))) == 0)
 		return (0);
 	if (content == 0)
 		content_size = 0;

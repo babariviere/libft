89ae2cbe54d5d4a761551d53734d8b2336afb653
babariviere <babariviere@protonmail.com>
2017-12-11 13:07:25 +0100

    skip until

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 16:51:56 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/11 12:11:59 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/11 13:05:25 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,7 @@ char			**ft_strsplit(const char *s, char c);
 void			ft_strsort(char **strs, int (*cmp)(const char *, const char *));
 void			ft_strremove(char **strs, size_t idx);
 void			ft_strdedup(char **strs);
+void			ft_skip_until(char **str, char c);
 
 /*
 ** CHARS

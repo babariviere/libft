4b17f5f010b2cb0d3cceb30aacc97b852245eac0
babariviere <babariviere@protonmail.com>
2017-12-12 11:20:12 +0100

    oops

--- src/ft_print_tab.c
+++ src/ft_print_tab.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 11:18:33 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/12 11:18:58 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/12 11:20:04 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_print_tab(const char **str)
 {
+	if (str == 0)
+		return ;
 	while (*str)
 	{
 		ft_putstr(*str);

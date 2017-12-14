be4cdfc553057fa6d4aaaa47e418f2a7161c980b
babariviere <babariviere@protonmail.com>
2017-12-14 13:40:55 +0100

    real fix

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 14:58:04 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/14 13:39:35 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/14 13:40:48 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (n == 0)
 		return (0);
-	while (n-- && *s1 == *s2)
+	while (n && *s1 == *s2)
 	{
 		if (*s1 == 0)
 			break ;
+		n--;
 		s1++;
 		s2++;
 	}
-	if (n == -1)
+	if (n == 0)
 		return (0);
 	return (*(const t_uchar *)s1 - *(const t_uchar *)s2);
 }

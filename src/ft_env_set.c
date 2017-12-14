1d6125700b2c89d199e4f462c126204fc7592f6a
babariviere <babariviere@protonmail.com>
2017-12-14 13:28:34 +0100

    fix

--- src/ft_env_set.c
+++ src/ft_env_set.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 09:07:29 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/14 11:59:01 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/14 13:28:28 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void			ft_env_set(char ***envp_ptr, char *name, char *value, int over)
 		}
 		idx++;
 	}
-	envp = ft_realloc(envp, idx, idx + 1);
+	envp = ft_realloc(envp, idx * sizeof(char *), (idx + 1) * sizeof(char *));
 	envp[idx] = 0;
 	envp[idx - 1] = ft_strjoin_sep(name, "=", value);
 	*envp_ptr = envp;

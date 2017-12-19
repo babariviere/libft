d5a01d2f102152454055c2f322f2e2f127dcbfe0
babariviere <babariviere@protonmail.com>
2017-12-19 13:55:48 +0100

    rec

--- include/ft_geo.h
+++ include/ft_geo.h
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_geo.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/20 16:51:56 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/19 13:55:09 by briviere         ###   ########.fr       */
+/*   Created: 2017/12/19 13:54:02 by briviere          #+#    #+#             */
+/*   Updated: 2017/12/19 13:55:29 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef FT_GEO_H
+# define FT_GEO_H
 
-# include "ft_chr.h"
-# include "ft_dlst.h"
-# include "ft_env.h"
-# include "ft_fmt.h"
-# include "ft_geo.h"
-# include "ft_gnl.h"
-# include "ft_lst.h"
-# include "ft_mem.h"
-# include "ft_nbr.h"
-# include "ft_io.h"
-# include "ft_str.h"
-# include "ft_term.h"
+#include <string.h>
+
+typedef struct	s_rect {
+	int		x;
+	int		y;
+	size_t	width;
+	size_t	height;
+}				t_rect;
 
 #endif

6c1d1c84099dafc991bae7b44cd40884ffe24ff7
babariviere <babariviere@protonmail.com>
2017-12-19 15:45:44 +0100

    fix

--- src/term/ft_term_ue.c
+++ src/term/ft_term_ue.c
@@ -6,17 +6,17 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 15:41:35 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/19 15:42:15 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/19 15:44:58 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_term.h"
 
-char	*ft_term_ue(char **termbuf)
+void	ft_term_ue(char **termbuf)
 {
 	static char	*ue;
 
 	if (ue == 0)
 		ue = tgetstr("ue", termbuf);
-	return (ue);
+	tputs(ue, 1, ft_fputchar);
 }
--- src/term/ft_term_us.c
+++ src/term/ft_term_us.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_term_ul.c                                       :+:      :+:    :+:   */
+/*   ft_term_us.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 15:34:27 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/19 15:41:02 by briviere         ###   ########.fr       */
+/*   Updated: 2017/12/19 15:45:20 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_term.h"
 
-char	*ft_term_us(char **termbuf)
+void	ft_term_us(char **termbuf)
 {
 	static char	*us;
 
 	if (us == 0)
 		us = tgetstr("us", termbuf);
-	return (us);
+	tputs(us, 1, ft_fputchar);
 }

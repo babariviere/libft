44af138b49763a4956b5e849fec8626822d9320e
babariviere <babariviere@protonmail.com>
2017-12-19 10:16:27 +0100

    cursor move

--- src/term/ft_term_cur_mv.c
+++ src/term/ft_term_cur_mv.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_term.h                                          :+:      :+:    :+:   */
+/*   ft_term_cur_mv.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/18 17:24:25 by briviere          #+#    #+#             */
-/*   Updated: 2017/12/19 10:13:16 by briviere         ###   ########.fr       */
+/*   Created: 2017/12/19 10:05:54 by briviere          #+#    #+#             */
+/*   Updated: 2017/12/19 10:15:56 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_TERM_H
-# define FT_TERM_H
+#include "ft_term.h"
 
-# include "ft_env.h"
-# include "ft_put.h"
-# include "ft_str.h"
-# include <term.h>
+void	ft_term_cur_mv(char **termbuf, int hpos, int vpos)
+{
+	static char		*cm;
 
-char	*ft_term_init(const char *termtype);
-char	*ft_term_type(void);
-int		ft_term_col(void);
-int		ft_term_row(void);
-void	ft_term_clr(char **termbuf);
-void	ft_term_cur_mv(char **termbuf, int hpos, int vpos);
-
-#endif
+	if (cm == 0)
+		cm = tgetstr("cm", termbuf);
+	ft_putstr(tgoto(cm, hpos, vpos));
+}
